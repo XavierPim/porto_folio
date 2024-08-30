017257471f759dd17b3c530c8958416ec9ace6cf
Xavier Pimentel <97432816+XavierPim@users.noreply.github.com>
2024-08-30 15:26:35 -0700

    new pages made. finished component side bar making connections from side bar to pages. active animation for sidebar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './pages/welcome.js';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Welcome from './pages/welcome.js';
 import Home from './pages/home.js';
+import Expertise from './pages/expertise.js'
+import Experience from './pages/experience.js'
+import Projects from './pages/projects.js'
+import Contact from './pages/contact.js'
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Routes>
       <Route path="/" element={<Welcome/>} />
       <Route path="/home" element={<Home/>} />
+      <Route path="/expertise" element={<Expertise/>} />
+      <Route path="/experience" element={<Experience/>} />
+      <Route path="/projects" element={<Projects/>} />
+      <Route path="/contact" element={<Contact/>} />
       </Routes>
     </div>
     </Router>
---
+++ src/components/sidebar.js
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import home_icon from "../components/icons/Home.png";
+import expertise_icon from "../components/icons/Wrench.png";
+import experience_icon from "../components/icons/Clock.png";
+import projects_icon from "../components/icons/Star.png";
+import contact_icon from "../components/icons/Phone.png";
+import "./sidebar.css";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1 className="name-plate">Xavier_Pimentel</h1>
+            <ul>
+                <li>
+                    <NavLink to="/home" activeClassName="active"><img src={home_icon} alt="home_icon" />Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/expertise" activeClassName="active"><img src={expertise_icon} alt="expertise_icon" />Expertise</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/experience" activeClassName="active"><img src={experience_icon} alt="experience_icon" />Experience</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/projects" activeClassName="active"><img src={projects_icon} alt="projects_icon" />Projects</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" activeClassName="active"><img src={contact_icon} alt="contact_icon" />Contact</NavLink>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
