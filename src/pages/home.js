44b6a761a5b7184380547007579f0f5da92e340a
Xavier Pimentel <97432816+XavierPim@users.noreply.github.com>
2024-08-30 20:25:17 -0700

    big screen added - not yet component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,19 @@
 import "../css/home.css"
 import Sidebar from "../components/sidebar";
+
+import xIcon from "../components/icons/X.png" 
 function Home(){
     return(
 <div className="home-container">
 <Sidebar/>
-<div className="content-container"></div>
+<div className="home-content-container">
+    <div className="big-screen">
+        <div className="topbar">
+            <div>/home</div>
+            <img src={xIcon} id="exit" alt="x-icon"/>
+        </div>
+    </div>
+</div>
 </div>
     );
 }
