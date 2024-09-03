a7f4c55b679505cbbae7d85c727d3c686b6e2529
Xavier Pimentel <97432816+XavierPim@users.noreply.github.com>
2024-09-03 11:44:45 -0700

    expertise page finished

--- src/pages/expertise.js
+++ src/pages/expertise.js
@@ -2,13 +2,14 @@ import "../css/expertise.css";
 import Sidebar from "../components/sidebar";
 import ContentBigScreen from "../components/content-big-screen";
 import tabs from "../components/text content/nav-tabs";
+import UML from "../content/Expertise_UML.png"
 function Expertise(){
     return(
 <div className="expertise-container">
 <Sidebar/>
 <div className="content-container">
     <ContentBigScreen tab_name={tabs.expertise}>
-
+<img src={UML} alt="UML" className="UML"/>
     </ContentBigScreen>
 </div>
 </div>
